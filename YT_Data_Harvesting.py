b4a3c51115e46cd3c3a66c0b429dca17d82c9fa1
roshanmurkar <85471797+roshanmurkar@users.noreply.github.com>
2023-07-04 18:00:21 +0530

    removed : duration conversion

--- YT_Data_Harvesting.py
+++ YT_Data_Harvesting.py
@@ -167,7 +167,7 @@ with col1:
                     # Duration format transformation (Duration format transformation function call)
                     duration = video.get('contentDetails', {}).get('duration', 'Not Available')
                     if duration != 'Not Available':
-                        duration = convert_duration(duration)
+                        duration = 'Not Available'
                     video['contentDetails']['duration'] = duration        
                                 
                     video_data.append(video)
@@ -188,19 +188,6 @@ with col1:
             response = request.execute()
             
             return response
-
-        # Define a function to convert duration
-        def convert_duration(duration):
-            regex = r'PT(\d+H)?(\d+M)?(\d+S)?'
-            match = re.match(regex, duration)
-            if not match:
-                return '00:00:00'
-            hours, minutes, seconds = match.groups()
-            hours = int(hours[:-1]) if hours else 0
-            minutes = int(minutes[:-1]) if minutes else 0
-            seconds = int(seconds[:-1]) if seconds else 0
-            total_seconds = hours * 3600 + minutes * 60 + seconds
-            return '{:02d}:{:02d}:{:02d}'.format(int(total_seconds / 3600), int((total_seconds % 3600) / 60), int(total_seconds % 60))
 
         # Function call to Get Videos data and comment data from video ids
         video_data = get_video_data(youtube, video_ids)
